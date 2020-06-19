58dfc5be68ed409bb4174db8c6973bdde3b9f3a3
Rupinder <rupindermonga@gmail.com>
2020-06-19 09:21:42 +0530

    final code for an input

--- final_word2sub.py
+++ final_word2sub.py
@@ -1,69 +1,12 @@
 import wordninja, os, re
 from math import log
 import math
-import enchant, time
 import json
 from heapq import nsmallest
 import pandas as pd
 
-# d = enchant.Dict("en_US")
-
-
-'''
-#old file writing into new
-with open("wordninja_words.txt") as f:
-    original_words = f.read().split()
-    # words = f.read().split()
-
-words = []
-with open("wordninja_words_dict.txt", 'w') as f:
-    for eachWord in original_words:
-        if d.check(eachWord) and len(eachWord) != 1:
-            words.append(eachWord)
-            f.write('%s\n' % eachWord)
-'''
-
-
-# d = enchant.Dict("en_US")
-# #old file writing into new
-# with open("wordninja_words.txt") as f:
-#     original_words = f.read().split()
-#     # words = f.read().split()
-
-# words = []
-# with open("wordninja_words_final.txt", 'w') as f:
-#     for eachWord in original_words:
-#         if len(eachWord) != 1 and d.check(eachWord):
-#             words.append(eachWord)
-#             f.write('%s\n' % eachWord)
-
-
-# # with open("wordninja_words.txt") as f:
-# #     original_words = f.read().split()
-
-
-# # words = []
-# # with open("wordninja_words_dict2.txt", 'w') as f:
-# #     for eachWord in original_words:
-# #         if len(eachWord) != 1:
-# #             words.append(eachWord)
-# #             f.write('%s\n' % eachWord)
-
-
-
-
 with open("wordninja_words_final.txt") as f:
     words = f.read().split()
-
-
-# def second_smallest(numbers):
-#     m1, m2 = (float('inf'), 1), (float('inf'), 1)
-#     for x in numbers:
-#         if x[0] <= m1[0]:
-#             m1, m2 = x, m1
-#         elif x[0] < m2[0]:
-#             m2 = x
-#     return m2
 
 def smallest(numbers):
     min_list = []
@@ -86,16 +29,12 @@ def smallest(numbers):
     position1 = min_list.index(second_min)
     position2 = min_list.index(third_min)
     return numbers[position1], numbers[position2] 
+
+
 # Build a cost dictionary, assuming Zipf's law and cost = -math.log(probability).
 # words = open("words-by-frequency.txt").read().split()
 
-
-# words = "camerastick"
-
 wordcost = dict((k, log((i+1)*log(len(words)))) for i,k in enumerate(words))
-
-# with open('result.json', 'w') as fp:
-#     json.dump(wordcost, fp)
 
 maxword = max(len(x) for x in words)
 
@@ -117,17 +56,12 @@ def infer_spaces(s):
         numbered1 = []
         for k1, c1 in candidates1:
             numbered1.append((c1 + wordcost.get(s[i-k1-1:i],9e999), k1+1))
-        # return min(numbered)
         count1 = sum(1 if x[0] < 9e999 else 0 for x in numbered1)
         
         if count1 == 1:
             final1 = min(numbered1)
         else:
             final1 = smallest(numbered1)[0]
-        # if len(numbered1)==1 or math.isinf(smallest(numbered1)[0][0]):
-        #     final1 = min(numbered1)
-        # else:
-        #     final1 = smallest(numbered1)[0]
         
         numbered2 = []
         for k2, c2 in candidates2:
@@ -142,22 +76,7 @@ def infer_spaces(s):
         else:
             final2 = min(numbered2)
             
-            # if not math.isinf(smallest(numbered1)[1][0]):
-            #     final2 = smallest(numbered2)[1]
-            # elif not math.isinf(smallest(numbered1)[0][0]):
-            #     final2 = smallest(numbered2)[0]
-            # else:
-            #     final2 = min(numbered2)
-        # elif len(numbered2) == 2:
-        #     if not math.isinf(smallest(numbered1)[0][0]):
-        #         final2 = smallest(numbered2)[0]
-        #     else:
-        #         final2 = min(numbered2)
-        # else:
-        #     final2 = min(numbered2)
-
         return final0, final1, final2
-
 
     # Build the cost array.
     
@@ -197,25 +116,7 @@ def infer_spaces(s):
         out2.append(s[i-k2:i])
         i -= k2
 
-
-
     return s, " ".join(reversed(out0)), " ".join(reversed(out1)), " ".join(reversed(out2))
 # s ="photostick"
 s = "aftereffect"
-# s = 'smartphone'
-# s="thumbgreenappleactiveassignmentweeklymetaphor"
 print(infer_spaces(s))
-
-start_time = time.time()
-with open("compound_words.txt") as f:
-    compoundWords = f.read().split()
-
-final_result = []
-for eachWord in compoundWords:
-    final_result.append(infer_spaces(eachWord))
-df = pd.DataFrame(final_result, columns = ["compound_word", "split_1", "split_2", "split_3"])
-
-df.to_csv('final_result7.csv')
-
-print(time.time()-start_time)
-
